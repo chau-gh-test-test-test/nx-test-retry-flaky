d0e73abf7b9b117111ba0e583f2ff2b053ca0d67
nartc <nartc7789@gmail.com>
2024-12-05 13:56:12 -0600

    failed

--- apps/nx-test-retry-flaky/src/app/app.spec.tsx
+++ apps/nx-test-retry-flaky/src/app/app.spec.tsx
@@ -10,6 +10,6 @@ describe('App', () => {
 
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
-    expect(getByText(/Welcome nx-test-retry-flaky/gi)).toBeTruthy();
+    expect(getByText('failed')).toBeTruthy();
   });
 });
