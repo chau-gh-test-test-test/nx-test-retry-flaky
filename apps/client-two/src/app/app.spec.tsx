587dbd8504d7e1f360f68928d8888958caa7c882
nartc <nartc7789@gmail.com>
2024-12-05 14:45:24 -0600

    failed me

--- apps/client-two/src/app/app.spec.tsx
+++ apps/client-two/src/app/app.spec.tsx
@@ -10,6 +10,6 @@ describe('App', () => {
 
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
-    expect(getByText(/Welcome client-two/gi)).toBeTruthy();
+    expect(getByText('failed me')).toBeTruthy();
   });
 });
