8cefb1121f1f6c66cddfaecf713e5735a2188812
nartc <nartc7789@gmail.com>
2024-12-05 10:29:18 -0600

    Initial commit

---
+++ apps/nx-test-retry-flaky/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'nx-test-retry-flaky',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/nx-test-retry-flaky',
+};
